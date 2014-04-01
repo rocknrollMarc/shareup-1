154d8b1e5106ef6e28770cfd9d7997c033e02380
Levent Gurses <levent.gurses@64clicks.com>
2014-03-31 23:33:28 -0400

    Share service

--- app/assets/javascripts/app/services.js
+++ app/assets/javascripts/app/services.js
@@ -22,4 +22,31 @@ angular.module('myApp.services', [])
       };
 
       return service;
-    });
+  })
+
+  .factory('Share', function($resource) {
+     var Share = $resource('/api/shares/:id.json',
+         {id: '@id'},
+         {}
+     );
+  })
+
+  .factory('SessionService', function($http, $q) {
+     var service = {
+         getCurrentUser: function() {
+             if (service.isAuthenticated()) {
+                 return $q.when(service.currentUser);
+             } else {
+                 return $http.get('/api/current_user').then(function(response) {
+                     return service.currentUser = response.data;
+                 });
+             }
+         },
+         currentUser: null,
+         isAuthenticated: function() {
+             return !!service.currentUser;
+         }
+     };
+
+     return service;
+  });
